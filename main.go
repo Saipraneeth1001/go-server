d4809800f09b5d87e86cd86ab191d8412823850d
saipraneeth1001 <saipraneeth0448@gmail.com>
2022-07-12 09:38:42 +0530

    Added hello func handler. Added filserver and served index.html in static folder.

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 import (
 	"fmt"
-	
+	"log"
+	"net/http"
 )
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello Rudra Deva")
+}
 
 func main() {
 
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+
 	fmt.Printf("Starting server at port 8080\n");
+	err := http.ListenAndServe(":8080", nil); 
+	if err != nil {
+        log.Fatal(err)
+    }
+
+
+
 }
