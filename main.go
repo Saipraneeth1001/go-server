96fb0868b8a02427101fc1a13970f2dad921dddc
saipraneeth1001 <saipraneeth0448@gmail.com>
2022-07-12 09:53:16 +0530

    Added basic post request Added form.html in static and added its handler

--- main.go
+++ main.go
@@ -10,11 +10,27 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello Rudra Deva")
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm();
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Fprintf(w, "post request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "name = %s\n", name)
+	fmt.Fprintf(w, "address = %s\n", address)
+}
+
 func main() {
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("Starting server at port 8080\n");
 	err := http.ListenAndServe(":8080", nil); 
